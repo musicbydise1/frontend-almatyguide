3df3b241d41421c3131e669feb96f1b853703182
Aksultan Yessenbay <aksultane912@icloud.com>
2022-11-12 04:49:57 +0600

    ag

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./assets/Components/Header";
+import Home from "./assets/Pages/Home";
 
 function App() {
   return (
     <div className="App">
-     <Header />
     </div>
   );
 }
